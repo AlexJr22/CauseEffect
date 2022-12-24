6c606c83e75ac972eca142f20ed7fa64158522b3
Alex Junior <alexjr2oficial@gmail.com>
2022-12-24 19:24:51 -0300

    criando outro pessoa

--- src/script/Pessoas.js
+++ src/script/Pessoas.js
@@ -2,12 +2,21 @@ const pessoas = [
   {
     nome: 'Alex',
     sobrenome: 'Junior',
-    dadaDeNascimento: 12/11/2001,
+    dadaDeNascimento: '12/11/2001',
     idade: 21,
     telef: '(10) 93213-9876',
     estado: 'CE',
     pais: 'Brasil'
+  },
+  {
+    nome: 'Ezequiel',
+    sobrenome: 'BW',
+    dadaDeNascimento: 'xx/xx/xxxx',
+    idade: 22,
+    telef: '(10) 93213-9876',
+    estado: 'MG',
+    pais: 'Brasil'
   }
 ]
 
-export { pessoas }
+export { pessoas };
