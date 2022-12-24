cb81f7a3bae59a58e500cbe0995284b515d86e2a
Alex Junior <alexjr2oficial@gmail.com>
2022-12-24 19:07:11 -0300

    criando um arrey de objetos

--- src/script/Pessoas.js
+++ src/script/Pessoas.js
@@ -1,6 +1,13 @@
-
-const pessoas = {
-  Ola: () => console.log('Olá Mundo!')
-}
+const pessoas = [
+  {
+    nome: 'Alex',
+    sobrenome: 'Junior',
+    dadaDeNascimento: 12/11/2001,
+    idade: 21,
+    telef: '(10) 93213-9876',
+    estado: 'CE',
+    pais: 'Brasil'
+  }
+]
 
 export { pessoas }
