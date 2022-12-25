1c8ee32acbe25829e72014cdeb4fc2ef84b50b89
Alex Junior <alexjr2oficial@gmail.com>
2022-12-25 12:11:54 -0300

    adicionando um script que renderiza o s  card dinamicamente

--- src/script/script.js
+++ src/script/script.js
@@ -4,11 +4,9 @@ let aside = document.getElementById('Pessoas');
 
 console.log(aside.innerHTML);
 pessoas.forEach(pessoa => {
-  console.log(pessoa.nome);
-  console.log(pessoa.sobrenome);
-  console.log(pessoa.dadaDeNascimento);
-  console.log(pessoa.idade);
-  console.log(pessoa.telef);
-  console.log(pessoa.estado);
-  console.log(pessoa.pais);
+
+  let cardPessoa = document.createElement("div");
+  cardPessoa.innerHTML = `<div class="Pessoa card">${pessoa.nome} ${pessoa.sobrenome}</div>`
+
+  aside.appendChild(cardPessoa);
 });
