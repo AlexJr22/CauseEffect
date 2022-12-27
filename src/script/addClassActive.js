80ea29785e82d6ddb953d52466125824ebd424bb
Alex Junior <alexjr2oficial@gmail.com>
2022-12-27 20:20:09 -0300

    criando um script para adicionanr a class='active'

--- src/script/addClassActive.js
+++ src/script/addClassActive.js
@@ -1,4 +1,13 @@
 const active = (element) => {
   console.log(element);
+
+  for (let o = Pessoas.children.length; o > 0; o--) {
+
+    Pessoas.childNodes[o].firstChild.classList.remove('active');
+
+  };
+  
   element.classList.add('active');
 };
+
+console.log(Pessoas.childNodes);
